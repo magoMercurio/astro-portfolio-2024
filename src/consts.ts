3e3cca0c4a3911620f09ab37ce668fccf951e350
mago_d_Mercurio <davila.feria@gmail.com>
2024-02-13 22:14:46 -0600

    fixing

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Raul DevWeb';
+export const SITE_TITLE = 'Raúl WebDesign';
 export const SITE_DESCRIPTION = 'desarrollo web a medida';
